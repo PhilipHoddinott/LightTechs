3a5b874216e623887c4d4622088dfdcdc73df6fc
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2016-02-23 15:39:33 -0500

    work

--- Labs/Lab2/LAB2draft2.c
+++ Labs/Lab2/LAB2draft2.c
@@ -32,8 +32,11 @@ void debounce(void);
 unsigned char random(void);		//random interger function
 void StopAndResetTimer(void);
 void StartTimer(void);
-void PauseTimer(void);
+void Pause(void);
 void ending(void);
+void system_initilization(void); //initalizze systems
+
+void BILEDOFF(void);
 unsigned char rand(void);
 unsigned LEDPAT(void)
 //-----------------------------------------------------------------------------
@@ -61,31 +64,36 @@ unsigned char Points [3][3]=;
 //main program function
 //******************************************************************************************
 void main(void) {
-	Sys_Init();      			// System Initialization
-	Port_Init();     			// Initialize ports 2 and 3
-	Interrupt_Init();			//Initialize Interrupts
-	Timer_Init();				//Initialize timer 0
-	putchar(' ');    		
-	printf("\r\nStart\r\n");
+	system_initilization();
 	while(1) {	
-		BILED0=1;	//Turn OFF the BILED
-		BILED1=1;
-		printf("\rHow To Play:\r\nIf Red Led is on the player must Push button 0\r\nIf Green Led is on the player must Push button 1\r\nIf both Leds are on the player must push both Buttons\r\nKeep in mind you will only have 1 sec push the corresponding button/s\r\nA correct input will make the Clear Led flash green, while an incorrect input will make it flash red\r\nTo begin press any key\r\n");	
+		BILEDOFF();
+		printf("\rPlay game\n");
 
 		keyinput=getchar();
 		
 		previousnum=keyinput%3;
-		i=0;
 		numC=0;
+
+		//for get turns just get the memory working
+		for(int i=0;i<10;i++){
+			int k =0;
+			while (k<=i){
+				seq(i, rand[k]);
+			}// goes through as many tuurns to the next one
+			//i is the number of les we will be ligthing
+			//light led
+			//wait time
+			//if led is right then well done, then repert with one more 
+		}
+
 		while (i<=9) {
 			StopAndResetTimer();
 			if(!SS) {
 				StartTimer();
-				randomnum = random();					//create a random number. where randomnum is a 
-				while (randomnum==previousnum) {
-					randomnum = random();
-				}							//check to see if the random number was used before
-	
+				// play sequence
+				// if first was right then go on if wrong then end turn
+				
+				rand[i];	
 				if (randomnum==0) {						
 					LED0=0;					//turn it on
 					debounce(); 			
@@ -116,23 +124,75 @@ void main(void) {
 						incorrect();
 					}
 				}
-				
-				previousnum=randomnum;
 				i++;									//increment i
 			} else 	{
-				PauseTimer();
+				Pause();
 			}
 
-		}// end while < 9
-		printf("\r\n Number of correct answers = %d\r\n", numC);		//display results
+		}// end while < 9	//display results
 		ending();
 		TR0 = 0;								//turn off timer
-		BILED1=1;
-		BILED0=1;
 		while (!SS);							//wait until the switch is turned off and back on again to loop
-		while (SS);								
+		while (SS); // the toggle						
 	}	
 }//end main
+
+
+//****************************************************************************************
+// Philip made functions, sort as you would
+//*****************************************************************************************
+
+
+
+void seq(int turns, int led){
+	light(led);
+	//wait the time
+
+
+	//light led
+	//wait for button
+	//if correct light next led
+	//else break
+}
+
+void light(int led){
+	LED0=0;
+	LED1=0;
+	LED2=0;
+	LED3=0;
+	switch(led){
+		case 0:
+			LED0=0;
+			break;
+		case 1:
+			LED1=0;
+			break;
+		case 2:
+			LED2=0;
+			break;
+		case 3:
+			LED3=0;
+			break;
+	}//end swich stament
+}
+
+
+
+
+
+
+
+void system_initilization(void){ // this does all the sys init stuff
+	Sys_Init();      			// System Initialization
+	Port_Init();     			// Initialize ports 2 and 3
+	Interrupt_Init();			//Initialize Interrupts
+	Timer_Init();				//Initialize timer 0
+	putchar(' ');    		
+	printf("\r\nStart\r\n");
+}
+
+
+
 
 //****************************************************************************************
 // Port_Init - Initializes Ports 2 and 3 in the desired modes for input and output and Interrupt
@@ -140,8 +200,7 @@ void main(void) {
 
 // re initialize the ports depending on the setup 
 
-/*void Port_Init(void)
-{
+/*void Port_Init(void){
 	// Port 3
 	P3MDOUT |= 0xF8; //set Port 3 output pins to push-pull mode
 	P3MDOUT &= 0xFC; //set Port 3 input pins to open drain mode
@@ -153,25 +212,27 @@ void main(void) {
 	P2 |= ~0XFE;     //set Port 2 input pin to high impedance state
 
 }*/
-
-
+//******************************************************************************************
+//LED Functions
+//******************************************************************************************
+void BILEDOFF (void){
+	BILED1=1;
+	BILED0=1;
+}
 //******************************************************************************************
 //Timer Function
 //******************************************************************************************
-void StopAndResetTimer(void)
-{
+void StopAndResetTimer(void) {
     TR0 = 0;           	// Stop Timer0
     TMR0 = 0;           // Clear high & low byte of T0	
 	Counts=0;			// clear counts
 }
-void Interrupt_Init(void)
-{
+void Interrupt_Init(void) {
     IE |= 0x02;      // enable Timer0 Interrupt request (by masking)
     EA = 1;       // enable global interrupts (by sbit)
 }
 //***************
-void Timer_Init(void)
-{
+void Timer_Init(void) {
 
     CKCON |=0x00;  // Timer0 uses SYSCLK as source
     TMOD &=0xF0;   	// clear the 4 least significant bits
@@ -181,15 +242,13 @@ void Timer_Init(void)
 
 }
 
-void Analog_init(void)
-{
+void Analog_init(void){
 	REF0CN = 0x03; 	//Set Vref to use internal reference voltage (2.4 V)
 	ADC1CN = 0x80;		//Enable A/D converter (ADC1) 
 	ADC1CF |= 0x01; 	//Set A/D converter gain to 1 
 }
 
-unsigned char ReadAnalog(unsigned char n)
-{
+unsigned char ReadAnalog(unsigned char n){
 	AMX1SL = n; 					//Set P1.n as the analog input for ADC1 
 	ADC1CN = ADC1CN & ~0x20; 		//Clear the [Conversion Completed] flag 
 	ADC1CN = ADC1CN | 0x10; 		//Initiate A/D conversion 
@@ -211,15 +270,13 @@ void Timer0_ISR(void) __interrupt 1
 */
 
 /*return a random integer number between 0 and 1*/
-unsigned char random(void)
-{
+unsigned char random(void) {
     return (rand()%4);  // rand returns a random number between 0 and 32767.
 }
 
 //***************
 
-unsigned char rand(void)
-{
+unsigned char rand(void) {
 	previousnum=keyinput%3;
 				x=0;
 				// wait 1/10s 
@@ -234,8 +291,7 @@ unsigned char rand(void)
 	x++;
 	}
 }
-void resetArrays(void)				
-{
+void resetArrays(void) {
 
 	for(x=0; x<=2; x++)
 	{
@@ -256,48 +312,46 @@ unsigned LEDPAT(void)
 	resetArrays();
 	rand();
 }
-void debounce(void)
-{
+void debounce(void){
 	countHolder=counts;				//store starting counts
 	while((counts-countHolder)<4); //wait 15ms 
 					
 }
 
-void StopAndResetTimer(void)
-{
+void StopAndResetTimer(void){
     TR0 = 0;           	// Stop Timer0
     TMR0 = 0;           // Clear high & low byte of T0	
 	counts=0;			// clear counts
 }
 
-void StartTimer(void)
-{
+void StartTimer(void){
 	TR0=1;
 }
 
-void PauseTimer(void)
-{
+void PauseTimer(void){
 	TR0=0;
+	while(SS){
+		BILED0=1;
+	}//
+
+
 }
 
-void correct(void)
-{
+void correct(void){
 	BILED0 = 1;
 	BILED1 = 0;
 	numC++;
 	debounce();
 }
 
-void incorrect(void)
-{
+void incorrect(void){
 	BILED0 = 0;
 	BILED1 = 1;
 	Buzzer=0;
 	debounce();
 	Buzzer=1;
 }
-void ending(void)
-{
+void ending(void){
 	Buzzer=0;
 	Counts=0;			//store starting counts
 	while(Counts<(338/2)); 
